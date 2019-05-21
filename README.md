b97441c6d2cecc8cc8301b53b79f24ebe5dc4e1b
connor <connor.jaksik@gmail.com>
2019-05-21 15:16:54 -0600

    version two completed styling

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 https://jaksik.github.io/ml-color-classifier/
 
-A "Hello World" app using Tensorflow JS to train a model to predict a specified color.
-When the page is loaded the model automatically starts training its self to associate a color name with certain RGB values.
-It is constantly trying to predict the color chosen by the user on the canvas.
-The color names on the screen are the programs predictions of the color on the canvas.
-Notice that after 30 seconds to a minute the computers predictions begin to be much more accurate.
+Using TensorFlow JS and a data set of rgb values with color labels to train a model to guess the name of a color created by the user.
+
+When the page first loads, use the R/G/B sliders to create your custom color.
+Notice how the computer's prediction is far from accurate.
+Then hit the "Train Model" button.
+You will notice that the computer's prediction gradually improves in accuracy.
+The model is programmed to train for 10 epochs, which will take about one minute to complete.
+When finished training the accuracy is near 100%.
--- logic.js
+++ logic.js
@@ -1,11 +1,116 @@
-// window.addEventListener('DOMContentLoaded', (event) => {
-//     console.log('DOM fully loaded and parsed');
+let data;
+let xs, ys;
+let model;
+let labelP;
+let lossP;
+let label;
 
-//     const data = loadJSON('https://raw.githubusercontent.com/CodingTrain/ColorClassifer-TensorFlow.js/master/colorData.json');
-// });
+//list of labels the classifier can choose from
+let labelList = [
+  'red-ish',
+  'green-ish',
+  'blue-ish',
+  'orange-ish',
+  'yellow-ish',
+  'pink-ish',
+  'purple-ish',
+  'brown-ish',
+  'grey-ish'
+]
+
+//importing the data used to train the model
+function preload() {
+  data = loadJSON('https://raw.githubusercontent.com/CodingTrain/ColorClassifer-TensorFlow.js/master/colorData.json');
+}
+
+function setup() {
+  labelP = createP('');
+  lossP = createP('');
+  console.log(data.entries.length);
+  let colors = [];
+  let labels = [];
+  for (let record of data.entries) {
+    let col = [record.r / 255, record.g / 255, record.b / 255];
+    //pushing RGB values to array
+    colors.push(col);
+    //pushing RGB labels to array with as index value
+    labels.push(labelList.indexOf(record.label));
+  }
+  // creating a 2d tensor
+  xs = tf.tensor2d(colors);
+
+  let labelsTensor = tf.tensor1d(labels, 'int32');
+  labelsTensor.print();
+
+  ys = tf.oneHot(labelsTensor, 9);
+  labelsTensor.dispose();
+
+  console.log(xs.shape);
+  console.log(ys.shape);
+
+  xs.print();
+  ys.print();
+
+  model = tf.sequential();
+
+  let hidden = tf.layers.dense({
+    units: 16,
+    activation: 'sigmoid',
+    inputDim: 3
+  });
+  let output = tf.layers.dense({
+    units: 9,
+    activation: 'softmax'
+  });
+  model.add(hidden);
+  model.add(output);
+
+  //create optimizer
+  const lr = 0.2;
+  const optimizer = tf.train.sgd(lr);
+
+  model.compile({
+    optimizer: optimizer,
+    loss: 'categoricalCrossentropy'
+  });
+}
+
+//Actually training our model, programmed for 10 epochs
+function train() {
+  const options = {
+      epochs: 10,
+      validationSplit: 0.1,
+      shuffle: true,
+      callbacks: {
+          onTrainBegin: () => console.log('training start'),
+          onTrainEnd: () => console.log('training complete'),
+          onBatchEnd: tf.nextFrame,
+          onEpochEnd: (num, logs) => {
+              console.log('Epoch: ' + num);
+              document.getElementById("loss").innerHTML = logs.loss;
+              console.log('Loss: ' + logs)
+          }
+      }
+  }
+  return model.fit(xs, ys, options)
+}
+
+//
+function draw() {
+  tf.tidy(() => {
+      const xs = tf.tensor2d([
+          [r / 255, g / 255, b / 255]
+      ]);
+      let results = model.predict(xs);
+      let index = results.argMax(1).dataSync()[0];
+
+      label = labelList[index];
+      document.getElementById("prediction").innerHTML = label;
+  });
+}
 
 
-
+//slider value change event listener
 
 var rSlider = document.getElementById("rSlider");
 var gSlider = document.getElementById("gSlider");
@@ -15,19 +120,42 @@ var rValue = document.getElementById("rValue");
 var gValue = document.getElementById("gValue");
 var bValue = document.getElementById("bValue");
 
+var r = 127.5;
+var g = 127.5;
+var b = 127.5;
+
 rValue.innerHTML = rSlider.value;
 gValue.innerHTML = gSlider.value;
 bValue.innerHTML = bSlider.value;
 
-
-rSlider.oninput = function() {
+rSlider.oninput = function () {
+  r = this.value;
   rValue.innerHTML = this.value;
+  updateCanvas();
 }
 
-gSlider.oninput = function() {
-    gValue.innerHTML = this.value;
-  }
+gSlider.oninput = function () {
+  g = this.value;
+  gValue.innerHTML = this.value;
+  updateCanvas();
+}
 
-bSlider.oninput = function() {
-    bValue.innerHTML = this.value;
-  }
+bSlider.oninput = function () {
+  b = this.value;
+  bValue.innerHTML = this.value;
+  updateCanvas();
+}
+
+function updateCanvas() {
+  r.toString();
+  g.toString();
+  b.toString();
+
+  var rgb = "rgb(" + r + ", " + g + ", " + b + ")";
+
+  document.getElementById("canvas").style.backgroundColor = rgb;
+  document.getElementById("rgb-value").innerHTML = rgb;
+
+}
+
+updateCanvas();
